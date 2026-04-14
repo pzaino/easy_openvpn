b555531f39aa575abab988604eadaba9345e9701
Paolo Fabio Zaino <p.zaino@zfpsystems.com>
2026-04-14 17:43:48 +0100

    Added build automation

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+rm -rf ./openvpn/
+
 mkdir -p openvpn/{server,pki,clients,ccd}
 docker compose build
+
+docker compose run --rm openvpn init-pki
+
+docker compose run --rm openvpn add-client my-client
+
 docker compose run --rm openvpn bash
-
-
